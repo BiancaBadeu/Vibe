ca2ab8ea1103069d9b8fc04cf8993c610500261b
Eli3336 <92146656+Eli3336@users.noreply.github.com>
2021-12-07 15:25:53 +0100

    Add files via upload

---
+++ SEP1/sep/src/model/DateTime.java
@@ -0,0 +1,111 @@
+package model;
+
+public class DateTime
+{
+  private int day;
+  private int month;
+  private int year;
+  private int hour;
+  private int minute;
+
+  public DateTime(int day, int month, int year, int hour, int minute)
+  {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+    this.hour = hour;
+    this.minute = minute;
+  }
+
+  public void setDay(int day)
+  {
+    this.day = day;
+  }
+  public void setMonth(int month)
+  {
+    this.month = month;
+  }
+  public void setYear(int year)
+  {
+    this.year = year;
+  }
+  public void setHour(int hour)
+  {
+    this.hour = hour;
+  }
+  public void setMinute(int minute)
+  {
+    this.minute = minute;
+  }
+
+  public int getDay()
+  {
+    return day;
+  }
+  public int getMonth()
+  {
+    return month;
+  }
+  public int getYear()
+  {
+    return year;
+  }
+  public int getHour()
+  {
+    return hour;
+  }
+  public int getMinute()
+  {
+    return minute;
+  }
+
+  public boolean isAfter(DateTime time)
+  {
+    if(this.year>time.year)
+      return true;
+    else if(this.year<time.year)
+      return false;
+    else
+    {
+      if(this.month>time.month)
+        return true;
+      else if(this.month<time.month)
+        return false;
+      else
+      {
+        if(this.day>time.day)
+          return true;
+        else if(this.day<time.day)
+          return false;
+        else
+        {
+          if(this.hour>time.hour)
+            return true;
+          else if(this.hour<time.hour)
+            return false;
+          else
+          {
+            if(this.minute>time.minute)
+              return true;
+            else
+              return false;
+          }
+        }
+      }
+    }
+  }
+
+  public String toString()
+  {
+    return "Date: " + this.day + "/" + this.month + "/" + this.year + ", time: " + this.hour + ":" + this.minute;
+  }
+
+  public boolean equals(Object obj)
+  {
+    if(!(obj instanceof DateTime))
+      return false;
+    DateTime other = (DateTime) obj;
+    return this.day == other.day && this.month == other.month && this.year == other.year
+        && this.hour == other.hour && this.minute == other.minute;
+  }
+}
---
+++ SEP1/sep/src/readers/RoomReader.java
@@ -0,0 +1,41 @@
+package readers;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+import model.*;
+
+public class RoomReader
+{
+
+  public static void main(String[] args) throws Exception
+  {
+    /* file variable is created with the data from the text file */
+    File file = new File("C:\\Users\\luisd\\Downloads\\Rooms.txt");
+
+    Scanner in = new Scanner(file);
+    ArrayList<Room> roomLists= new ArrayList<>();
+    while (in.hasNext())
+    {
+
+      String line = in.nextLine();
+      String[] splittingline = line.split(",");
+      String roomNumber = splittingline[0].trim();
+      int roomSize = Integer.parseInt(splittingline[1].trim());
+      String roomNumber2= splittingline[3].trim();
+      Room room;
+
+      if(roomNumber2!=null){
+
+        room= new Room(roomNumber, roomSize, roomNumber2);
+
+      }
+      else
+        room= new Room(roomNumber, roomSize);
+
+      roomLists.add(room);
+    }
+
+  }
+
+}
