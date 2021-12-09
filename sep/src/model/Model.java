3337ffad4cab06ab6585f263cb7b651c0285c2b1
Eli3336 <92146656+Eli3336@users.noreply.github.com>
2021-12-09 16:35:24 +0100

    Add files via upload

---
+++ sep/src/model/Model.java
@@ -0,0 +1,45 @@
+package model;
+import readers.*;
+
+import java.util.ArrayList;
+
+public interface Model
+{
+  void inputFiles() throws Exception;
+  TeacherList getAllTeachers();
+  void addTeacher(Teacher teacher);
+  void removeTeacherFromSystemByID(String id);
+  void removeStudentFromSystemByID(int id);
+  Teacher getTeacherByID(String id);
+  TeacherList getTeachersByName(String name);
+  void addTeacherToCourse(Teacher teacher, Course course);
+  void removeTeacherFromCourseByID(String id, Course course);
+  CourseList getAllCourses();
+  void addCourse(Course course);
+  Course getCourseByID(String id);
+  void calculateNumberOfLessonsForCourse(Course course);
+  void removeStudentFromCourseByID(int id, Course course);
+  StudentList getAllStudents();
+  void addStudent(Student student);
+  void removeStudentByID(int id);
+  StudentList getAllStudentsInClassID(String classID);
+  StudentList getStudentsByName(String name);
+  SessionList getAllSessions();
+  void addSession(Session session);
+  void removeSession(Session session);
+  ArrayList<Session> getBookedSessions();
+  ArrayList<Session> getUnbookedSessions();
+  RoomList getAllRooms();
+  void addRoom(Room room);
+  Room getRoomByID(String id);
+  RoomList getRoomsBySize(int capacity);
+  RoomList getRoomsBySizeBiggerThan(int capacity);
+  RoomList getUnitableRooms();
+  ArrayList<Room> getRoomsAvailableInPeriod(DateTime startTime, DateTime endTime);
+  ArrayList<Room> getRoomsAvailableInPeriodBySize(int capacity, DateTime startTime, DateTime endTime);
+  ArrayList<Room> getRoomsAvailableInPeriodBySizeBiggerThan(int capacity, DateTime startTime, DateTime endTime);
+  ArrayList<Room> getUnitableRoomsInPeriod(DateTime startTime, DateTime endTime);
+  ArrayList<Room> getAllBookedRooms();
+  ArrayList<Room> getAllUnbookedRooms();
+  String toString();
+}
