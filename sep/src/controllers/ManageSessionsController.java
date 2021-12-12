46af71a01da81cccea16c0662f1072cfc5af38cc
domignat <92146634+domignat@users.noreply.github.com>
2021-12-12 16:16:12 +0100

    Update ManageSessionsController.java

--- sep/src/controllers/ManageSessionsController.java
+++ sep/src/controllers/ManageSessionsController.java
@@ -1,28 +1,48 @@
 package controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.Region;
-import model.Model;
-import view.ViewHandler;
+import model.*;
+
 
 public class ManageSessionsController
 {
+  @FXML private TableView<Session> tableView;
   private Region root;
-  private Model model;
-  private ViewHandler viewHandler;
-
-  public ManageSessionsController(){}
-
-  public void init(ViewHandler viewHandler, Model model, Region root)
+  private model.Model model;
+  private view.ViewHandler viewHandler;
+ ObservableList<Session> observableList= FXCollections.observableArrayList(
+      new SessionList().getAllSessions());
+  
+  public ManageSessionsController()
   {
-    this.viewHandler = viewHandler;
-    this.model = model;
-    this.root = root;
   }
-
+  public void init(view.ViewHandler viewHandler, model.Model model, Region root){
+    this.viewHandler= viewHandler;
+    this.model= model;
+    this.root= root;
+    tableView.setItems(observableList);
+  }
   public void reset(){}
 
   public Region getRoot()
   {
     return root;
   }
+  @FXML private void toAdd(){
+    viewHandler.openView("AddSession");
+  }
+  @FXML private void toRemove(){
+    viewHandler.openView("RemoveSession");
+  }
+  @FXML private void toEdit(){
+    viewHandler.openView("EditSession");
+  }
+  @FXML private void toCancel(){
+    viewHandler.openView("Start");
+  }
 }
+
