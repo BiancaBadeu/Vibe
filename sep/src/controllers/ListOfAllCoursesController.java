1e271b07cc7e9e4b648a9de62a009c4149a50428
Eli3336 <92146656+Eli3336@users.noreply.github.com>
2021-12-12 18:22:51 +0100

    Add files via upload

--- sep/src/controllers/ListOfAllCoursesController.java
+++ sep/src/controllers/ListOfAllCoursesController.java
@@ -1,6 +1,11 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Region;
+import model.Course;
 import model.Model;
 import view.ViewHandler;
 
@@ -10,6 +15,8 @@ public class ListOfAllCoursesController
   private Model model;
   private ViewHandler viewHandler;
 
+  @FXML TableView courseTable;
+
   public ListOfAllCoursesController(){}
 
   public void init(ViewHandler viewHandler, Model model, Region root)
@@ -17,7 +24,43 @@ public class ListOfAllCoursesController
     this.viewHandler = viewHandler;
     this.model = model;
     this.root = root;
+
+    TableColumn course = new TableColumn("Course name");
+    course.setCellValueFactory(new PropertyValueFactory<>("courseID"));
+    TableColumn ectS= new TableColumn("ECTS");
+    ectS.setCellValueFactory(new PropertyValueFactory<>("ects"));
+
+    courseTable.getColumns().setAll(course, ectS);
+    try
+    {
+      for (int i = 0; i < model.getAllCoursesAsArrayList().size(); i++)
+      {
+        courseTable.getItems().add(model.getAllCoursesAsArrayList().get(i));
+      }
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
   }
+
+  @FXML public void addStudentPressed()
+  {
+    int index = courseTable.getSelectionModel().getFocusedIndex();
+    if(index > -1)
+    {
+      Course course = model.getAllCoursesAsArrayList().get(index);
+      if(!(course.getStudentList().getAllStudentsAsArrayList().contains(AddStudentController.student)))
+        course.addStudent(AddStudentController.student);
+    }
+  }
+
+  @FXML public void donePressed()
+  {
+    viewHandler.openView("ManageStudentsAndTeachers");
+  }
+
+  @FXML public void filterPressed(){}
 
   public void reset(){}
 
