a65dd92a43f513e93e902a0d86e7cd4e932697b3
Xbey25 <92146595+Xbey25@users.noreply.github.com>
2021-12-14 19:23:51 +0100

    Update AddTeacherController.java

--- sep/src/controllers/AddTeacherController.java
+++ sep/src/controllers/AddTeacherController.java
@@ -4,9 +4,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.layout.Region;
+import model.Student;
+import model.StudentList;
 import model.Teacher;
+import model.TeacherList;
 import view.ViewHandler;
+
+import java.io.*;
 import java.util.Optional;
+import java.util.Scanner;
+
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -28,27 +35,53 @@ public class AddTeacherController
     this.root = root;
   }
 
-  @FXML void addTeacherButtonPressed()
+  @FXML void addTeacherButtonPressed() throws IOException
   {
-    System.out.println(model.getAllTeachers());
-    String name = nameField.getText();
-    String id = idField.getText();
-    Teacher teacher = new Teacher(name,id);
-    try
+    File file = new File("C:\\Users\\luisd\\IdeaProjects\\SEP1_V2_files\\src\\ourTxt\\teacherList.txt");
+
+    Scanner in = new Scanner(file);
+    TeacherList listOfTeachers= new TeacherList();
+
+    in.nextLine();
+
+    while (in.hasNext())
     {
-      model.validateAddTeacher(name, id);
-      errorLabel.setText("");
+
+
+      String line = in.nextLine();
+
+
+
+      String[] splittingline = line.split(",");
+      String nameTeacher = splittingline[0].trim();
+      String teacherID = splittingline[1].trim();
+
+      Teacher teacher = new Teacher(nameTeacher, teacherID);
+      listOfTeachers.addTeacher(teacher);
     }
-    catch (Exception e)
-    {
-      errorLabel.setText(e.getMessage());
+
+
+
+
+    Teacher newTeacher= new Teacher(nameField.getText(), idField.getText());
+    listOfTeachers.addTeacher(newTeacher);
+    model.addTeacher(newTeacher);
+    try {
+      PrintWriter myWriter = new PrintWriter("C:\\Users\\luisd\\IdeaProjects\\SEP1_V2_files\\src\\ourTxt\\teacherList.txt");
+
+
+      for (int i=0; i<model.getAllTeachersAsArrayList().size(); i++){
+
+        myWriter.print(model.getAllTeachersAsArrayList().get(i));
+
+      }
+      myWriter.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-    if(errorLabel.getText().equals(""))
-      if(booleanconfirmation())
-      {
-        model.addTeacher(teacher);}
-    reset();
-    System.out.println(model.getAllTeachers());
+
+
+    viewHandler.openView("ManageStudentsAndTeachers");
   }
 
   private boolean booleanconfirmation()
